aa5b757e876d753ef276dd3bc091cf81b11d8aba
Ryan MacLean <ryan@ryanmaclean.com>
2015-05-22 23:11:57 -0700

    Update httpdcount.sh
    Fixed the and operand, some logic in the greater than and lesser than operators.

--- httpdcount.sh
+++ httpdcount.sh
@@ -11,13 +11,13 @@ numrunning=`ps cax | grep $command | wc -l`
 #Send text to console depending on ammount of running processes
 
 #First case: between 1 and 10
-if [ $numrunning >= 1 -a $numrunning <= 10 ]; then display="[LOW] Web Server OK!";
+if [ $numrunning >= 1 -a $numrunning < 10 ]; then display="[LOW] Web Server OK!";
 
 #Second case: between 20 and 99
-elif [ $numrunning >= 20 -a $numrunning < 100 ]; then display="[HIGH] Web Server Working hard!";
+elif [ $numrunning > 20 -a $numrunning <= 100 ]; then display="[HIGH] Web Server Working hard!";
 
 #Third case: 100 processes or more
-elif [ $numrunning >= 100 ]; then display="[CRITICAL] Web Server under heavy load, restart required";
+elif [ $numrunning > 100 ]; then display="[CRITICAL] Web Server under heavy load, restart required";
 service $command restart
 
 #Fourth case: If it's less than 1 or between 11 and 20, exit
