8266705326c9d327f98a072c3dffd8bff1be417e
Ryan MacLean <ryan@ryanmaclean.com>
2015-05-24 15:15:29 -0700

    Update httpdcount.sh
    Changed operators to text-based operators, broke out text display and service restart from main loop. Added (commented out) scenario for 10~20 as part of testing.

--- httpdcount.sh
+++ httpdcount.sh
@@ -1,28 +1,46 @@
 #!/bin/bash
 
 #Set the name of the process you want to monitor. 
-#Apache2 is commented out, but might be needed depending on your platform
+#Apache2 is commented out, but might be needed depending on your platform 
+#E.g.: Debian family, used for testing
+
 #command="apache2"
 command="httpd"
 
-#Count the ammount of processes and store them in the "running" variable
+#Initialize restart variable to false as the command could be run more than once in a session
+restart=false
+
+#Count the ammount of processes and store them in the "numrunning" variable
 numrunning=`ps cax | grep $command | wc -l`
 
 #Send text to console depending on ammount of running processes
 
 #First case: between 1 and 10
-if [ $numrunning >= 1 -a $numrunning < 10 ]; then display="[LOW] Web Server OK!";
+if [ $numrunning -ge 1 -a $numrunning -lt 10 ]; then 
+	display="[LOW] Web Server OK!";
 
-#Second case: between 20 and 99
-elif [ $numrunning > 20 -a $numrunning <= 100 ]; then display="[HIGH] Web Server Working hard!";
+#elif [ $numrunning -gt 10 -a $numrunning -le 20 ]; then 
+#	display="[MED] Web Server Normal";
+
+#Second case: between 20 and 100
+elif [ $numrunning -gt 20 -a $numrunning -le 100 ]; then 
+	display="[HIGH] Web Server Working hard!";
 
 #Third case: 100 processes or more
-elif [ $numrunning > 100 ]; then display="[CRITICAL] Web Server under heavy load, restart required";
-service $command restart
+elif [ $numrunning -gt 100 ]; then 
+	display="[CRITICAL] Web Server under heavy load, restart required";
+	restart=true
 
-#Fourth case: If it's less than 1 or between 11 and 20, exit
+else
+#Exit gracefully if between 10 and 20
 exit 0;
-
-print $display
-
 fi
+
+#Restart service if required
+if $restart; then
+	restarttext="Restarting process"
+	service $command restart
+fi
+
+#Display the text if set
+echo $display
