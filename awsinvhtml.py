211abaab3e4a341971f7d45cf334e70457025e99
Ryan MacLean <ryan@ryanmaclean.com>
2015-05-24 16:45:09 -0700

    Create awsinvhtml.py
    Initial commit. Added user input to this script, but should switch it over to the bash script (per directives).

---
+++ awsinvhtml.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+import sys
+import subprocess
+print "Enter a file name:",
+filename = raw_input()
+print "Starting JSON Conversion to HTML"
+subprocess.call("./awsinv.sh", shell=True)
+sys.stdout = open(filename, 'w')
+print "Completed Conversion, HTML File Saved as " + filename
