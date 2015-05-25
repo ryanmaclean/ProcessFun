cc8c89acd83d9274a6fe2fdd5731e4f598c61839
Ryan MacLean <ryan@ryanmaclean.com>
2015-05-24 17:02:32 -0700

    Update awsinvhtml.py
    Commented out file operations, changed text. Working on JSON to HTML conversion with std libs.

--- awsinvhtml.py
+++ awsinvhtml.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 import sys
 import subprocess
-print "Enter a file name:",
-filename = raw_input()
+import json
+#print "Enter a file name:",
+#filename = raw_input()
 print "Starting JSON Conversion to HTML"
-subprocess.call("./awsinv.sh", shell=True)
-sys.stdout = open(filename, 'w')
-print "Completed Conversion, HTML File Saved as " + filename
+#subprocess.call("./awsinv.sh", shell=True)
+#sys.stdout = open(filename, 'w')
+print "Completed Conversion, HTML File Saved as aws.html"
