1455c225502480a2bf9e8f8f471d310ccff99890
Ryan MacLean <ryan@ryanmaclean.com>
2015-05-24 17:00:55 -0700

    Update awsinv.sh
    Added file i/o logic to bash script instead of python. Added filename variable and init in order to concatenate stdout for each command.

--- awsinv.sh
+++ awsinv.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-#Get info about EC2, ELB, RDS, Elasticache and Cloudformation for a particular region
-#Output defaults to STDOUT in JSON format
+#Get info about EC2, ELB, RDS, Elasticache and Cloudformation for a particular region.
+#Output defaults to variable file name in JSON format which we overwrite on each run.
 
 #Set the region for the commands
 region="us-east-1"
 
-aws ec2 describe-instances --region $region
-aws elb describe-load-balancers --region $region
-aws rds describe-db-instances --region $region
-aws elasticache describe-cache-clusters --region $region
-aws cloudformation describe-stacks --region $region
+#Set the exported file's name
+filename="aws.json"
+
+#Initialize the file
+: > $filename
+
+#Run the AWS CLI commands and concatenate contents of file
+aws ec2 describe-instances --region $region 1>>$filename
+aws elb describe-load-balancers --region $region 1>>$filename
+aws rds describe-db-instances --region $region 1>>$filename
+aws elasticache describe-cache-clusters --region $region 1>>$filename
+aws cloudformation describe-stacks --region $region 1>>$filename
