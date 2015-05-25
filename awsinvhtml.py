78de9e6765eb3aaa5f1b704558834dc58a59c782
Ryan MacLean <ryan@ryanmaclean.com>
2015-05-24 18:08:34 -0700

    Update awsinvhtml.py
    Added Safari task at the end to view file. 
    Noticed file is imported weirdly, appears to be 2.7 issue, working on unicode with v3 as a work-around (commented out) if I can't fix it. 
    
    Currently status is one portion (ONLY) of JSON output display in HTML file with "u/" replacing many line starts (known version issue).

--- awsinvhtml.py
+++ awsinvhtml.py
@@ -2,9 +2,34 @@
 import sys
 import subprocess
 import json
+#from pprint import pprint
 #print "Enter a file name:",
 #filename = raw_input()
 print "Starting JSON Conversion to HTML"
-#subprocess.call("./awsinv.sh", shell=True)
+subprocess.call("./awsinv.sh", shell=True)
+
+#Read JSON File
+with open('/Users/string/Desktop/aws.json') as data_file:    
+    data = json.load(data_file)
+#pprint(data)
+
+#data = []
+#with codecs.open('/Users/string/Desktop/aws.json','rU','utf-8') as f:
+#    for line in f:
+#       data.append(json.loads(line))
+
+#Create HTMNL File
+f = open('aws.html','w')
+message = """<html>
+<head></head>
+<body><p>AWS Details</p>"""+str(data)+"""</body>
+</html>"""
+f.write(message)
+f.close()
+
 #sys.stdout = open(filename, 'w')
 print "Completed Conversion, HTML File Saved as aws.html"
+
+#Remove once tested
+print "Opening file with Safari to test"
+subprocess.call("open -a Safari aws.html", shell=True)
